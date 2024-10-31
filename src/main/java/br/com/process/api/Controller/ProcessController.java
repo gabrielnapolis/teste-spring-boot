5e4d42a80143dec1bce0872200e31d91e95dcd26
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-10-30 22:06:17 -0300

    ProcessController

--- src/main/java/br/com/process/api/Controller/ProcessController.java
+++ src/main/java/br/com/process/api/Controller/ProcessController.java
@@ -1,12 +1,23 @@
 package br.com.process.api.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.process.api.Model.ProcessModel;
+
 @RestController
+@RequestMapping("/api")
 public class ProcessController {
     @GetMapping("")
     public String mensagem() {
         return "Hello World!";
+    }
+    
+    @PostMapping("/process")
+    public ProcessModel process(@RequestBody ProcessModel p){
+        return p;
     }
 }
