7b6dc31bbe5f723ebb94b21ddc1009892f672f7d
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-10-30 22:20:20 -0300

    create findAll and findByProcessNumber

--- src/main/java/br/com/process/api/Controller/ProcessController.java
+++ src/main/java/br/com/process/api/Controller/ProcessController.java
@@ -1,23 +1,59 @@
 package br.com.process.api.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.process.api.Model.ProcessModel;
+import br.com.process.api.Repository.ProcessRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class ProcessController {
-    @GetMapping("")
-    public String mensagem() {
-        return "Hello World!";
-    }
-    
+
+    @Autowired
+    private ProcessRepository processRepository;
+
     @PostMapping("/process")
-    public ProcessModel process(@RequestBody ProcessModel p){
-        return p;
+    public ResponseEntity<String> saveProcess(@RequestBody @Valid ProcessModel processDto) {
+        if (processRepository.existsByProcessNumber(processDto.getProcessNumber())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Processo n° " + processDto.getProcessNumber() + " já cadastrado.");
+        }
+        ProcessModel process = new ProcessModel();
+        process.setProcessNumber(processDto.getProcessNumber());
+        processRepository.save(process);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Processo n° " + processDto.getProcessNumber() + " salvo com sucesso!");
+    }
+
+    @GetMapping("/process")
+    public List<ProcessModel> getProcess() {
+        return processRepository.findAll();
+    }
+
+    @GetMapping("/process/{processNumber}")
+    public ProcessModel selectByNumber(@PathVariable String processNumber) {
+        return processRepository.findByProcessNumber(processNumber);
+    }
+
+    @DeleteMapping("/process/{processNumber}")
+    public ResponseEntity<String> deleteProcess(@PathVariable String processNumber) {
+
+        ProcessModel process = processRepository.findByProcessNumber(processNumber);
+        if (process == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Processo n° " + processNumber + " não encontrado.");
+        }
+        processRepository.delete(process);
+        return ResponseEntity.status(HttpStatus.OK).body("Processo n° " + processNumber + " excluído com sucesso.");
     }
 }
--- src/main/java/br/com/process/api/Repository/ProcessRepository.java
+++ src/main/java/br/com/process/api/Repository/ProcessRepository.java
@@ -1,11 +1,14 @@
 package br.com.process.api.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import br.com.process.api.Model.ProcessModel;
 
 @Repository
 public interface ProcessRepository extends JpaRepository<ProcessModel, Long> {
     boolean existsByProcessNumber(String processNumber);
+    List<ProcessModel> findAll();
+    ProcessModel findByProcessNumber(String processNumber);
 }
