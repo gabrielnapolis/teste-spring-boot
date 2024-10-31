0b083c2bc8af45b84e5a2912c24a894d1a2da86a
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-10-30 23:10:49 -0300

    create Edit Process

--- src/main/java/br/com/process/api/Controller/ProcessController.java
+++ src/main/java/br/com/process/api/Controller/ProcessController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +56,10 @@ public class ProcessController {
         }
         processRepository.delete(process);
         return ResponseEntity.status(HttpStatus.OK).body("Processo n° " + processNumber + " excluído com sucesso.");
+    }
+
+    @PutMapping("/process")
+    public ProcessModel edit(@RequestBody ProcessModel processDto) {
+        return processRepository.save(processDto);
     }
 }
