529e73b0f2d67f9a7847ad530be4fb636d9827ee
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-10-30 22:20:20 -0300

    create endpoint POST process

--- src/main/java/br/com/process/api/Controller/ProcessController.java
+++ src/main/java/br/com/process/api/Controller/ProcessController.java
@@ -1,23 +1,34 @@
 package br.com.process.api.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.process.api.Model.ProcessModel;
+import br.com.process.api.Repository.ProcessRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class ProcessController {
-    @GetMapping("")
-    public String mensagem() {
-        return "Hello World!";
-    }
-    
+
+    @Autowired
+    private ProcessRepository processRepository;
+
     @PostMapping("/process")
-    public ProcessModel process(@RequestBody ProcessModel p){
-        return p;
+    public ResponseEntity<String> saveProcess(@RequestBody @Valid ProcessModel processDto) {
+        if (processRepository.existsByProcessNumber(processDto.getProcessNumber())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Processo n° " + processDto.getProcessNumber() + " já cadastrado.");
+        }
+        ProcessModel process = new ProcessModel();
+        process.setProcessNumber(processDto.getProcessNumber());
+        processRepository.save(process);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Processo " + processDto.getProcessNumber() + " salvo com sucesso.");
     }
 }
