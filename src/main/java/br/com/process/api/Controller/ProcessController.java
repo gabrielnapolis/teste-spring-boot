f3a35d587f80084ce77642626ee3ad9d809e0170
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-10-30 21:45:35 -0300

    create process controller

---
+++ src/main/java/br/com/process/api/Controller/ProcessController.java
@@ -0,0 +1,12 @@
+package br.com.process.api.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProcessController {
+    @GetMapping("")
+    public String mensagem() {
+        return "Hello World!";
+    }
+}
