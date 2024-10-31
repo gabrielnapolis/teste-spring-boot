b98dcd7c38d8c6c1242eec6552a8cf93512dd09b
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-10-30 22:06:01 -0300

    create ProcessModel

---
+++ src/main/java/br/com/process/api/Model/ProcessModel.java
@@ -0,0 +1,54 @@
+package br.com.process.api.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "process")
+public class ProcessModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "process_number", nullable = false, unique = true)
+    private String processNumber;
+
+    @Column(name = "defendant_name")
+    private String defendantName;
+
+    public ProcessModel() {}
+
+    public ProcessModel(String processNumber, String defendantName) {
+        this.processNumber = processNumber;
+        this.defendantName = defendantName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProcessNumber() {
+        return processNumber;
+    }
+
+    public void setProcessNumber(String processNumber) {
+        this.processNumber = processNumber;
+    }
+
+    public String getDefendantName() {
+        return defendantName;
+    }
+
+    public void setDefendantName(String defendantName) {
+        this.defendantName = defendantName;
+    }
+}
